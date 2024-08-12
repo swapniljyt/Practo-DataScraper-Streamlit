d5423df2895df190bf3f3ca91d5b144fa1cfad54
swapniljyt <swapniljytkd888@gmail.com>
2024-08-12 10:34:13 +0000

    committ al

--- App.py
+++ App.py
@@ -10,18 +10,23 @@ st.header('Enter the input features:')
 location = st.text_input("Doctor Location:")
 specialization = st.text_input("Doctor Specialization:")
 pages=10
-
+progress_bar = st.progress(0)
+status_text = st.empty()
 if st.button('Scrap'):
     all_doctors=[]
     for page in range(pages+1):
         url=generator(location,specialization,page)
         data=doctor_scrapper(url)
+        progress = int((page / pages) * 100)
+        progress_bar.progress(progress)
+        status_text.text(f"Scraping page {page}...")
         if data:
-            st.write(f"Scraping page{page}...")
             all_doctors.extend(data)
         else:
             st.write("no doctor Found on this page")
             break
+    progress_bar.empty()
+    status_text.text("Scraping complete!")        
     st.write(pd.DataFrame(all_doctors))            
 
 

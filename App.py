1bac59821f6750d22458720c9ecf5e4d2a6ee2b5
swapniljyt <swapniljytkd888@gmail.com>
2024-08-12 10:08:19 +0000

    commit

--- App.py
+++ App.py
@@ -1,6 +1,7 @@
 from src.url_generator import generator
 from src.scrapper import doctor_scrapper
 import streamlit as st
+import pandas as pd
 
 
 st.title('Practo-Doctor-Datascrapper')
@@ -8,8 +9,23 @@ st.title('Practo-Doctor-Datascrapper')
 st.header('Enter the input features:')
 location = st.text_input("Doctor Location:")
 specialization = st.text_input("Doctor Specialization:")
+pages=10
 
 if st.button('Scrap'):
+    all_doctors=[]
+    for page in range(pages+1):
+        url=generator(location,specialization,page)
+        data=doctor_scrapper(url)
+        if data:
+            st.write(f"Scraping page{page}...")
+            all_doctors.extend(data)
+        else:
+            st.write("no doctor Found on this page")
+            break
+    st.write(pd.DataFrame(all_doctors))            
+
+
+
     url=generator(location,specialization,"1")
     data=doctor_scrapper(url)
     if data:
